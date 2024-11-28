151428f6b7deae704ca2bb21e6f5e04f32169679
Jennifer <jennifer881030@gmail.com>
2024-11-28 17:32:22 +0800

    feat: display place info on info card

--- src/app/components/InfoCard.tsx
+++ src/app/components/InfoCard.tsx
@@ -1,18 +1,30 @@
 'use client';
 
-export default function InfoCard() {
+import { PlaceInfoType } from '@/app/types/map';
+
+export default function InfoCard({ info }: { info: PlaceInfoType }) {
   return (
     <div className="fixed left-16 top-1/2 transform -translate-y-1/2 w-2/5 h-2/3 bg-gray-200 rounded text-black p-16">
-      <h2 className="text-xl font-bold">店家名稱</h2>
-      <a className="text-blue-700 underline underline-offset-auto" href="#">
-        網站連結
-      </a>
-      <ul className="mt-6">
-        <li>是否營業中：</li>
-        <li>評分：</li>
-        <li>地址：</li>
-        <li>電話：</li>
-      </ul>
+      {JSON.stringify(info) === '{}' ? (
+        <>請點擊咖啡廳以取得地點資訊</>
+      ) : (
+        <>
+          <h2 className="text-xl font-bold">{info.name}</h2>
+          <a
+            className="text-blue-700 underline underline-offset-auto"
+            href={info.website}
+            target="_blank"
+          >
+            🔗 Link
+          </a>
+          <ul className="mt-6">
+            {info.open_now ? <li>營業中</li> : <li>休息中</li>}
+            <li>評分：{info.rating}</li>
+            <li>地址：{info.formatted_address}</li>
+            <li>電話：{info.formatted_phone_number}</li>
+          </ul>
+        </>
+      )}
     </div>
   );
 }
---
+++ src/app/types/map.ts
@@ -0,0 +1,8 @@
+export type PlaceInfoType = {
+  name?: string;
+  open_now?: boolean;
+  formatted_address?: string;
+  formatted_phone_number?: string;
+  rating?: number;
+  website?: string;
+};
